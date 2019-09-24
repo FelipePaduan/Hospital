2234e3552b402e4c3ca963a6e1ade53c352c5402
DJ_Juraski <daironrjuraski@gmail.com>
2019-09-24 11:05:59 -0300

    Add files via upload

---
+++ Hospital/test/dao/DAOTest.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import java.util.List;
+import modelo.Administrador;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author CC09059259980
+ */
+public class DAOTest {
+    
+    public DAOTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+//    @Test
+//    public void testinserir() {
+//        Administrador adm = new Administrador("talco", "Elias Talco", "1234");
+//        DAO<Administrador> dao = new DAO (Administrador.class);
+//        dao.inserir(adm);
+//    }
+    
+    @Test
+    public void testListar(){
+        List<Administrador> lista = new DAO(Administrador.class).listarTodos();
+        for(Administrador adm : lista){
+            System.out.println(adm);
+        }
+    }
+    
+}
