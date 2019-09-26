45e75e4b07823076b7a51ab75bfdf6d9d01bee35
DJ_Juraski <daironrjuraski@gmail.com>
2019-09-26 11:40:45 -0300

    Add files via upload

---
+++ Hospital/src/java/modelo/Animal.java
@@ -0,0 +1,99 @@
+package modelo;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Objects;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+
+@Entity
+public class Animal implements Serializable {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Integer id;
+    
+    @Column(length = 30)
+    private String nome;
+    
+    @ManyToOne
+    @JoinColumn(name = "dono")
+    private Dono dono;
+
+// construtores    
+    public Animal() {
+        this.nome = "";
+        this.dono = new Dono();
+    }
+    
+    public Animal(String nome, Dono dono) {
+        this.nome = nome;
+        this.dono = dono;
+    }
+
+//gets and seters
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Dono getDono() {
+        return dono;
+    }
+
+    public void setDono(Dono dono) {
+        this.dono = dono;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 17 * hash + Objects.hashCode(this.id);
+        hash = 17 * hash + Objects.hashCode(this.nome);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Animal other = (Animal) obj;
+        if (!Objects.equals(this.nome, other.nome)) {
+            return false;
+        }
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" + "id=" + id + ", nome=" + nome + '}';
+    }   
+}
